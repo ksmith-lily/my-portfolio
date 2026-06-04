a2134cf9ae4a962edc043a198771123c45f754a4
ksmith25x <ksmith@arrl.org>
2026-06-04 16:10:02 -0400

    style: improve nav and footer layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import Container from "./Container";
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-100 dark:bg-zinc-950 text-gray-900 dark:text-gray-100 py-6">
-          <Container>
+        <footer className="bg-gray-100 dark:bg-zinc-950 text-gray-900 dark:text-gray-100 py-6 px-4 sm:px-6 lg:px-8">
+
             <div className="relative inline-block text-center font-bold text-9xl tracking-widest">
               Lorem Ipsum
               <div className="absolute left-0 right-0 bottom-3 h-10 bg-gradient-to-t from-black/30 via-black/10 to-transparent dark:from-white/10 dark:via-white/5" />
@@ -12,7 +12,7 @@ export default function Footer() {
                 <div>Lorem ipsum</div>
                 <div>&copy; {new Date().getFullYear()} All rights reserved.</div>
             </div>
-          </Container>
+  
         </footer>
     );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,8 @@ import ThemeToggle from "./ThemeToggle";
 function Navbar() {
 
   return (
-    <nav className="border-t border-b border-gray-200 dark:border-zinc-800 sticky top-0 bg-gray-100 dark:bg-zinc-950 z-10">
-      <Container>
+    <nav className="border-t border-b border-gray-200 dark:border-zinc-800 sticky top-0 bg-gray-100 dark:bg-zinc-950 z-10 px-4 sm:px-6 lg:px-8">
+
         <div className="flex justify-between items-center py-3 text-black dark:text-gray-400">
           <div className="text-xl font-semibold text-black dark:text-white">Lorem Ipsum</div>
           <ul className="flex gap-4">
@@ -15,7 +15,7 @@ function Navbar() {
             <ThemeToggle />
           </ul>
         </div>
-      </Container>
+
     </nav>
   );
 }
