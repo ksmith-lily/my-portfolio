b58b765a32bcd736fc4a40be6f2fe42d40770c15
ksmith25x <ksmith@arrl.org>
2026-06-05 15:43:58 -0400

    feat: update route behavior

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Footer from "../components/Footer";
 import Home from "../pages/Home"
@@ -12,7 +12,8 @@ function App() {
       <div className="bg-white dark:bg-[rgb(9,8,12)] text-gray-900 dark:text-gray-100">
         <Navbar />
         <Routes>
-        <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
       <Footer />
